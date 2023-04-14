7017eb11736626399be538a9a58fa360f06aee6c
kumo0621 <kumokumokumo0621@gmail.com>
2023-04-14 22:49:18 +0900

    一部変更

--- src/main/java/com/github/kumo0621/ainana/OpenAIAsync.java
+++ src/main/java/com/github/kumo0621/ainana/OpenAIAsync.java
@@ -1,11 +1,15 @@
 package com.github.kumo0621.ainana;
+
 import org.bukkit.Bukkit;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.concurrent.CompletableFuture;
+
 public class OpenAIAsync {
     static boolean run = false;
+
     public static CompletableFuture<String> openaiAsync(String text, String name) {
         return CompletableFuture.supplyAsync(() -> {
             try {
@@ -19,25 +23,28 @@ public class OpenAIAsync {
 
     static String openai(String text, String name) throws Exception {
         run=true;
-        String hostname = "localhost"; // Pythonサーバーのホスト名
-        int port = 1111; // Pythonサーバーのポート番号
+        String hostname = ""; // Pythonサーバーのホスト名
+        int port = ; // Pythonサーバーのポート番号
 
         Socket socket = new Socket(hostname, port);
 
         // データの送信
         OutputStream output = socket.getOutputStream();
-        PrintWriter writer = new PrintWriter(output, true);
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, StandardCharsets.UTF_8), true);
         writer.println(text);
 
         // データの受信
         InputStream input = socket.getInputStream();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(input, StandardCharsets.UTF_8));
         byte[] response = reader.readLine().getBytes();
-        String spawn = new String(response, java.nio.charset.Charset.forName("SHIFT_JIS"));
+        String spawn = new String(response, java.nio.charset.StandardCharsets.UTF_8);
         System.out.println("Pythonからの応答：" + spawn);
 
         // ソケットのクローズ
+        input.close();
+        output.close();
         socket.close();
+
         run=false;
         Bukkit.broadcastMessage("<" + name + "> " + text);
         Bukkit.broadcastMessage("<NANA_0321> " + spawn);
